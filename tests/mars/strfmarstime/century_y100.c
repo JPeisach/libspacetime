af8d1bd1a2b7cc4e414d4bbf309adf2218297417
Joshua Peisach <jpeisach@ubuntu.com>
2026-06-29 14:07:54 -0400

    strfmarstime tests: %C (century)
    Signed-off-by: Joshua Peisach <jpeisach@ubuntu.com>

---
+++ tests/mars/strfmarstime/century_y100.c
@@ -0,0 +1,14 @@
+#include <libspacetime/mars.h>
+#include <stdio.h>
+
+int main()
+{
+    struct mars_tm tm;
+    tm.mars_tm_year = 100;
+
+    char str[10];
+    strfmarstime(str, sizeof(str), "%C", &tm);
+
+    printf("%s", str);
+    return 0;
+}
