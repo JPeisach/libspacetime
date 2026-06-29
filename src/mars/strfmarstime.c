c358cdff1da4d256e313c039a390b5361ecf85ff
Joshua Peisach <jpeisach@ubuntu.com>
2026-06-29 08:06:01 -0400

    strfmarstime: move gettext include to inside C file
    It won't be accessible to others in the header file.
    
    Signed-off-by: Joshua Peisach <jpeisach@ubuntu.com>

--- src/mars/strfmarstime.c
+++ src/mars/strfmarstime.c
@@ -6,6 +6,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef ENABLE_NLS
+#include "../gettext.h"
+#include <libintl.h>
+#include <locale.h>
+
+#define N_(STRING) gettext_noop(STRING)
+#else
+#define N_(STRING) STRING
+#endif
+
+// TODO: Gettext strings will have this as an array for accessing the different locales, maybe?
+static const char* month_names[24] = {
+    N_("Sagittarius"), N_("Dhanus"), N_("Capricornus"), N_("Makara"), N_("Aquarius"), N_("Kumbha"), N_("Pisces"), N_("Mina"), N_("Aries"), N_("Mesha"), N_("Taurus"), N_("Rishabha"), N_("Gemini"), N_("Mithuna"), N_("Cancer"), N_("Karka"), N_("Leo"), N_("Simha"), N_("Virgo"), N_("Kanya"), N_("Libra"), N_("Tula"), N_("Scorpius"), N_("Vrishika")
+};
+
+static const char* abbreviated_month_names[24] = {
+    N_("Sag"), N_("Dha"), N_("Cap"), N_("Mak"), N_("Aqu"), N_("Kum"), N_("Pis"), N_("Min"), N_("Ari"), N_("Mes"), N_("Tau"), N_("Ris"), N_("Gem"), N_("Mit"), N_("Can"), N_("Kar"), N_("Leo"), N_("Sim"), N_("Vir"), N_("Kan"), N_("Lib"), N_("Tul"), N_("Sco"), N_("Vri")
+};
+
+static const char* day_names[7] = {
+    N_("Solis"), N_("Lunae"), N_("Martis"), N_("Mercurii"), N_("Jovis"), N_("Veneris"), N_("Saturni")
+};
+
+static const char* abbreviated_day_names[7] = {
+    N_("So"), N_("Lu"), N_("Ma"), N_("Me"), N_("Jo"), N_("Ve"), N_("Sa")
+};
+
 // Implicit function declaration fix
 size_t __strfmarstime(char* restrict s, size_t count, const char* restrict format, const struct mars_tm* restrict tm, locale_t locale);
 
