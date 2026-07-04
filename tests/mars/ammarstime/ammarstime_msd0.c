df9d1d3d620c5162e228417b56cd9506e48396fb
Joshua Peisach <jpeisach@ubuntu.com>
2026-07-04 10:50:08 -0400

    tests: introduce ammarstime tests, starting with MSD 0
    Signed-off-by: Joshua Peisach <jpeisach@ubuntu.com>

---
+++ tests/mars/ammarstime/ammarstime_msd0.c
@@ -0,0 +1,14 @@
+#include <libspacetime/mars.h>
+#include <stdio.h>
+
+int main()
+{
+    mars_time_t time = 0;
+    struct mars_tm* tm = ammarstime(&time);
+
+    char str[100];
+    strfmarstime(str, sizeof(str), "%F %T", tm);
+    printf("%s", str);
+
+    return 0;
+}
