eb21a05155b65ee75dfd6e76d7df7c1f577fae74
Joshua Peisach <jpeisach@ubuntu.com>
2026-07-05 15:00:22 -0400

    mpfr support test
    Signed-off-by: Joshua Peisach <jpeisach@ubuntu.com>

--- src/mars/ammarstime.c
+++ src/mars/ammarstime.c
@@ -3,6 +3,10 @@
 #include "../const.h"
 #include <math.h>
 
+#ifdef HAVE_MPFR
+#include <mpfr.h>
+#endif
+
 #define SOLS_PER_2Y (668 + 669)
 #define SOLS_PER_10Y (668*4 + 669*6) // would be even, but skip if divisible by 10
 #define SOLS_PER_100Y ((10 * SOLS_PER_10Y) - 1) // skip if divisible by 100, so subtract 1
@@ -22,18 +26,26 @@ struct mars_tm* ammarstime(const mars_time_t* timer)
     static struct mars_tm ret;
     int months;
 
+#ifdef HAVE_MPFR
+    mpf_set_default_prec(50);
+    mpfr_t msd, new_timer;
+    mpfr_init(msd);
+    mpfr_init(new_timer);
+    mpfr_set_d(new_timer, *timer, MPFR_RNDZ);
+    mpfr_div_ui(msd, new_timer, 86400.0, MPFR_RNDN);
+
+    int days = ((int)mpfr_get_d(msd, MPFR_RNDN) + (8132745597 / SECONDS_PER_DAY));
+#else
     // First, get MSD (days)
     double msd = *timer / 86400.0;
-
-    // asked google and used its AI results for equations
-    // seconds into day:
-    int secs_into_day = *timer % SECONDS_PER_DAY;
-
 
     // When we refer to Darian Calendar days, MSD 0 will be offset.
     // Make sure we start at the right epoch
     int days = ((int)msd + (8132745597 / SECONDS_PER_DAY));
-
+#endif
+    // asked google and used its AI results for equations
+    // seconds into day:
+    int secs_into_day = *timer % SECONDS_PER_DAY;
 
     // Going based off how musl calculates secs to tm via "cycles"
     // Instead of 4Y -> 100Y -> 400Y, will do 2Y -> 10Y -> 100Y
@@ -77,16 +89,44 @@ struct mars_tm* ammarstime(const mars_time_t* timer)
     ret.mars_tm_wsol = wsol;
     ret.mars_tm_ysol = ysol;
 
+    ret.mars_tm_min = (secs_into_day % 3600) / 60;
+#ifdef HAVE_MPFR
+    // define what we need according to the non-mpfr version below
+    mpfr_t mtc, top, a, b, multwofourmsd, x, y, sixty, fin;
+    mpfr_init(mtc);
+    mpfr_init(top);
+    mpfr_init(multwofourmsd);
+    mpfr_init(x);
+    mpfr_init(y);
+    mpfr_init(fin);
+
+    mpfr_init_set_d(a, (double)3600.0, MPFR_RNDN);
+    mpfr_init_set_d(b, (double)24.0, MPFR_RNDN);
+    mpfr_init_set_d(sixty, (double)60.0, MPFR_RNDN);
+    mpfr_mul(multwofourmsd, msd, b, MPFR_RNDN);
+    mpfr_fmod(mtc, multwofourmsd, b, MPFR_RNDN);
+
+    mpfr_mul(x, mtc, a, MPFR_RNDN);
+    mpfr_fmod(y, x, a, MPFR_RNDN);
+
+    ret.mars_tm_hour = (int) mpfr_get_d(multwofourmsd, MPFR_RNDN) % 24;
+
+    mpfr_fmod(fin, y, sixty, MPFR_RNDN);
+    ret.mars_tm_sec = mpfr_get_d(fin, MPFR_RNDN); // why are we off by a second
+
+#else
     // The source of marsclock.com does these calculations for MSD -> MTC.
     // For more precision, use fmod.
     // Whether to round these numbers or not.. still undecided.
     double mtc = fmod(24 * msd, 24);
     double x = mtc * 3600;
-    double y = fmod(x, 3600);
 
     ret.mars_tm_hour = (int) (24 * msd) % 24;     // from marsclock.com
+    double y = fmod(x, 3600);
     ret.mars_tm_min = (secs_into_day % 3600) / 60;
     ret.mars_tm_sec = fmod(y, 60); // why are we off by a second
+#endif
+
 
     ret.mars_tm_amtoff = 0;
     ret.mars_tm_zone = "MTC";
--- src/mars/mars_time.c
+++ src/mars/mars_time.c
@@ -3,6 +3,10 @@
 #include "../const.h"
 #include "../earth.h"
 
+#ifdef HAVE_MPFR
+#include <mpfr.h>
+#endif
+
 inline double __do_earth_time_to_msd(time_t et)
 {
     return ((et + TAI_OFFSET) / 88775.244147) + 34127.2954262;
@@ -10,6 +14,33 @@ inline double __do_earth_time_to_msd(time_t et)
 
 inline mars_time_t __do_earth_time_to_mars_time(time_t et)
 {
+#ifdef HAVE_MPFR
+    // mpf_set_default_prec(50);
+    mpfr_t num, denom, base, msd, offset, ret, seconds_per_day;
+    mpfr_init(num);
+    mpfr_init(denom);
+    mpfr_init(base);
+    mpfr_init(msd);
+    mpfr_init(seconds_per_day);
+    mpfr_init(offset);
+    mpfr_init(ret);
+
+    mpfr_set_d(num, ((double)et + TAI_OFFSET), MPFR_RNDN);
+    mpfr_set_d(denom, (double)88775.244147, MPFR_RNDN);
+
+    mpfr_div(base, num, denom, MPFR_RNDN);
+
+    mpfr_set_d(offset, (double)34127.2954262, MPFR_RNDN);
+
+    mpfr_add(msd, base, offset, MPFR_RNDN);
+
+    mpfr_set_d(seconds_per_day, (double)86400.0, MPFR_RNDN);
+
+    mpfr_mul(ret, msd, seconds_per_day, MPFR_RNDN);
+
+    return mpfr_get_d(ret, MPFR_RNDN);
+
+#endif
     // martian seconds
     return (mars_time_t) (__do_earth_time_to_msd(et) * SECONDS_PER_DAY);
 }
