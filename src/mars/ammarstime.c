a27d83685c8329e4bc0afbbb685878bf7f651c68
Joshua Peisach <jpeisach@ubuntu.com>
2026-07-07 09:30:37 -0400

    ammarstime: get seconds into day through fmod
    I assumed that secs_into_day was fine. But I found that - and I don't
    know what was going on - the secs_into_day would be greater than just
    multiplying the remainder of the MSD by 86400 seconds.
    
    Don't even bother using the original timer. Just take the MSD and
    run with it.
    
    Signed-off-by: Joshua Peisach <jpeisach@ubuntu.com>

--- src/mars/ammarstime.c
+++ src/mars/ammarstime.c
@@ -25,10 +25,14 @@ struct mars_tm* ammarstime(const mars_time_t* timer)
     // First, get MSD (days)
     double msd = *timer / 86400.0;
 
-    // asked google and used its AI results for equations
     // seconds into day:
-    int secs_into_day = *timer % SECONDS_PER_DAY;
-
+    // Hey kids, this is why you DON'T always trust AI!
+    // If MSD is a fraction, we can literally use the remainder to determine how
+    // much of the day is left in seconds.
+    // After all, it's a whole out of 1.
+    // I managed to reduce a whole second of error because of this. After I don't know how long of manual debugging.
+    // int secs_into_day = *timer % SECONDS_PER_DAY;
+    int secs_into_day = fmod(msd, 1) * 86400;
 
     // When we refer to Darian Calendar days, MSD 0 will be offset.
     // Make sure we start at the right epoch
@@ -82,16 +86,9 @@ struct mars_tm* ammarstime(const mars_time_t* timer)
     ret.mars_tm_wsol = wsol;
     ret.mars_tm_ysol = ysol;
 
-    // The source of marsclock.com does these calculations for MSD -> MTC.
-    // For more precision, use fmod.
-    // Whether to round these numbers or not.. still undecided.
-    double mtc = fmod(24 * msd, 24);
-    double x = mtc * 3600;
-    double y = fmod(x, 3600);
-
     ret.mars_tm_hour = (int) (24 * msd) % 24;     // from marsclock.com
     ret.mars_tm_min = (secs_into_day % 3600) / 60;
-    ret.mars_tm_sec = fmod(y, 60); // why are we off by a second
+    ret.mars_tm_sec = (secs_into_day % 60); // why are we off by a second
 
     ret.mars_tm_amtoff = 0;
     ret.mars_tm_zone = "MTC";
