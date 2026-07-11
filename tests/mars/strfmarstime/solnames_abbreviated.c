ae40668fa02f1e117eb05184058b7223ab58a831
Joshua Peisach <jpeisach@ubuntu.com>
2026-07-11 19:45:17 -0400

    tests: use three day abbreviated sol names
    Signed-off-by: Joshua Peisach <jpeisach@ubuntu.com>

--- tests/mars/strfmarstime/solnames_abbreviated.c
+++ tests/mars/strfmarstime/solnames_abbreviated.c
@@ -3,41 +3,41 @@
 
 int main()
 {
-    char str[4];
+    char str[5];
 
     struct mars_tm solis;
     solis.mars_tm_wsol = 0;
-    strfmarstime(str, 4, "%a ", &solis);
+    strfmarstime(str, 5, "%a ", &solis);
     printf("%s", str);
 
     struct mars_tm lunae;
     lunae.mars_tm_wsol = 1;
-    strfmarstime(str, 4, "%a ", &lunae);
+    strfmarstime(str, 5, "%a ", &lunae);
     printf("%s", str);
 
     struct mars_tm martis;
     martis.mars_tm_wsol = 2;
-    strfmarstime(str, 4, "%a ", &martis);
+    strfmarstime(str, 5, "%a ", &martis);
     printf("%s", str);
 
     struct mars_tm mercurii;
     mercurii.mars_tm_wsol = 3;
-    strfmarstime(str, 4, "%a ", &mercurii);
+    strfmarstime(str, 5, "%a ", &mercurii);
     printf("%s", str);
 
     struct mars_tm jovis;
     jovis.mars_tm_wsol = 4;
-    strfmarstime(str, 4, "%a ", &jovis);
+    strfmarstime(str, 5, "%a ", &jovis);
     printf("%s", str);
 
     struct mars_tm veneris;
     veneris.mars_tm_wsol = 5;
-    strfmarstime(str, 4, "%a ", &veneris);
+    strfmarstime(str, 5, "%a ", &veneris);
     printf("%s", str);
 
     struct mars_tm saturni;
     saturni.mars_tm_wsol = 6;
-    strfmarstime(str, 4, "%a ", &saturni);
+    strfmarstime(str, 5, "%a ", &saturni);
     printf("%s", str);
 
     return 0;
