edaa6a5979589afff50fe780ce0a75bda311b539
Joshua Peisach <jpeisach@ubuntu.com>
2026-07-11 19:59:38 -0400

    tests: wcsfmarstime %y: fix format specifier
    Signed-off-by: Joshua Peisach <jpeisach@ubuntu.com>

--- tests/mars/wcsfmarstime/year_without_century_wide.c
+++ tests/mars/wcsfmarstime/year_without_century_wide.c
@@ -7,16 +7,16 @@ int main()
     wchar_t str[10];
     struct mars_tm singledigit;
     singledigit.mars_tm_year = 3;
-    wcsfmarstime(str, sizeof(str), L"%Y", &singledigit);
+    wcsfmarstime(str, sizeof(str), L"%y", &singledigit);
     printf("%ls ", str);
 
     struct mars_tm doubledigit;
     doubledigit.mars_tm_year = 34;
-    wcsfmarstime(str, sizeof(str), L"%Y", &doubledigit);
+    wcsfmarstime(str, sizeof(str), L"%y", &doubledigit);
     printf("%ls ", str);
 
     struct mars_tm tripledigit;
     tripledigit.mars_tm_year = 345;
-    wcsfmarstime(str, sizeof(str), L"%Y", &tripledigit);
+    wcsfmarstime(str, sizeof(str), L"%y", &tripledigit);
     printf("%ls", str);
 }
