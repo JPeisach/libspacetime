dc4316f3240432ffa9b307d77e29c061959579a0
Joshua Peisach <jpeisach@ubuntu.com>
2026-07-16 21:01:52 -0400

    Document mkmarstime
    Signed-off-by: Joshua Peisach <jpeisach@ubuntu.com>

--- src/mars/mkmarstime.c
+++ src/mars/mkmarstime.c
@@ -2,6 +2,7 @@
 
 #include "../const.h"
 
+// Returns the number of Martian Seconds since MSD 0 that equate to the broken down martian time structure (tm).
 mars_time_t mkmarstime(struct mars_tm* tm)
 {
     // TODO: Timezones
