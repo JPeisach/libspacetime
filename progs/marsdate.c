6e50e106e1d01db11c96ee3cff2a84dfc2d7d778
Joshua Peisach <jpeisach@ubuntu.com>
2026-07-27 09:58:17 -0400

    marsdate: support custom formatting
    Signed-off-by: Joshua Peisach <jpeisach@ubuntu.com>

--- progs/marsdate.c
+++ progs/marsdate.c
@@ -1,4 +1,5 @@
 #include <libspacetime/mars.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -23,6 +24,29 @@ int main(int argc, char** argv)
                 // RFC 5322 format
                 format = "%a, %d %b %Y %H:%M:%S %z";
                 break;
+        }
+    }
+
+    // If there are other stuff left to parse its either info to set
+    // date (or use as a base for formatting?), or define format.
+    // (util-linux seems to check this, and this is my first time using
+    // optind)
+    if(argc > optind) {
+        // Rule of the date command that I learned (feels weird to be
+        // making a program based off one I don't know how to use) -
+        // a + must be the only choice
+        // Learning by example proves to be useful once again.
+        if(argc > optind + 1) {
+            printf("marsdate: extra operand '%s'", argv[optind + 1]);
+            return 1;
+        }
+
+        if(argv[optind][0] == '+') {
+            // got a %, let rest of input be the format
+            // util-linux does this.
+            // the optind++ makes sense, I guess, to ensure the index is kept accurate
+            // the +1 is to get to the character after the +
+            format = argv[optind++] + 1;
         }
     }
 
